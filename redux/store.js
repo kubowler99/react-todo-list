69bc1fe1b1b63760e44be1b82356ed01223adcf4
kubowler99 <kubowler99@gmail.com>
2017-03-01 13:41:01 -0600

    video 11 work

--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import rootReducer from './reducers'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
 // TODO: add middleware
 
 let finalCreateStore = compose(
-    applyMiddleware(logger())
+    applyMiddleware(thunk, logger())
 )(createStore)
 
 export default function configureStore(initialState = {todos: [], user: {} }) {
